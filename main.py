e034ed4ae9bde634b7c60b8b6b24b19e0bed22d5
TylerWerne40 <TylerWerne40@gmail.com>
2025-01-03 23:20:16 -0700

    Drawing player on screen!

--- main.py
+++ main.py
@@ -3,22 +3,27 @@
 # throughout this file
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     window = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+    
     print("Starting asteroids!")
     print(f'Screen width: {SCREEN_WIDTH}')
     print(f'Screen height: {720}')
+    Clock = pygame.time.Clock()
+    dt = 0
+    p = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
     while(True):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-
         pygame.Surface.fill(window, (0, 0, 0))
 
+        p.draw(window)
         pygame.display.flip()
-        
+        dt = Clock.tick(60)/1000
 
 if __name__ == "__main__":
     main()
